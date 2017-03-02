c8977ce21ab14ca5378702bc4773a2be3ba19bbc
mtwtkman <punipuniomochi@gmail.com>
2017-03-02 23:25:28 +0900

    rusting

--- editor/src/main.rs
+++ editor/src/main.rs
@@ -1,40 +1,20 @@
 extern crate iron;
-#[macro_use(router)]
+#[macro_use]
 extern crate router;
-// #[macro_use]
-// extern crate tera;
-// extern crate iron_tera;
-extern crate handlebars_iron;
-extern crate handlebars;
-extern crate serde_json;
+#[macro_use]
+extern crate tera;
+extern crate iron_tera;
 
 use iron::prelude::*;
 use iron::status;
-use router::Router;
-// use tera::Context;
-// use iron_tera::{Template, TeraEngine, TemplateMode};
-use handlebars_iron::{Template, HandlebarsEngine, DirectorySource};
-use serde_json::value::{Value, Map};
-use handlebars::to_json;
-
-fn make_data() -> Map<String, Value> {
-    let mut data = Map::new();
-    data.insert("key".to_string(), Value::String("hoge".to_string()));
-    data
-}
+use tera::Tera;
 
 fn index(_: &mut Request) -> IronResult<Response> {
-    let mut resp = Response::new();
-    let data: Map<String, Value> = make_data();
-    resp.set_mut(Template::new("index", data)).set_mut(status::Ok);
-    Ok(resp)
+    Ok(Response::with((status::Ok, "hello")))
 }
 
 fn main() {
+    let tera = compiletemplates!("templates/**/*");
     let router = router!(index: get "/" => index);
-    let mut hbse = HandlebarsEngine::new();
-    hbse.add(Box::new(DirectorySource::new("./templates/", ".hbs")));
-    let mut chain = Chain::new(router);
-    chain.link_after(hbse);
-    Iron::new(chain).http("0.0.0.0:3000").unwrap();
+    Iron::new(router).http("0.0.0.0:3000").unwrap();
 }
