cbf5b646ff55d2b5639f804b86c2173b5e5faddc
mtwtkman <punipuniomochi@gmail.com>
2017-03-07 23:01:36 +0900

    add test

--- editor/src/main.rs
+++ editor/src/main.rs
@@ -1,20 +1,44 @@
 extern crate iron;
 #[macro_use]
 extern crate router;
-#[macro_use]
 extern crate tera;
 extern crate iron_tera;
+#[cfg(test)]
+extern crate iron_test;
 
 use iron::prelude::*;
 use iron::status;
-use tera::Tera;
+use tera::Context;
+use iron_tera::{TeraEngine, Template, TemplateMode};
 
 fn index(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, "hello")))
+    let mut response = Response::new();
+    let mut context = Context::new();
+    context.add("data", &"This is data");
+    response.set_mut(Template::new("index.html", TemplateMode::from_context(context)))
+            .set_mut(status::Ok);
+    Ok(response)
 }
 
 fn main() {
-    let tera = compiletemplates!("templates/**/*");
     let router = router!(index: get "/" => index);
-    Iron::new(router).http("0.0.0.0:3000").unwrap();
+    let mut chain = Chain::new(router);
+    let teng = TeraEngine::new("/source/src/templates/**/*");
+    chain.link_after(teng);
+    Iron::new(chain).http("0.0.0.0:3000").unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use iron::status;
+    use iron::headers::Headers;
+    use iron_test::request;
+
+    use super::index;
+
+    #[test]
+    fn test_index() {
+        let response = request::get("http://localhost:3000", Headers::new(), &index).unwrap();
+        assert_eq!(response.status.unwrap(), status::Status::Ok);
+    }
 }
