cc036af9d4ddb72a71b6d79fb908187022865d66
mtwtkman <punipuniomochi@gmail.com>
2017-04-05 23:03:20 +0900

    design

--- blog_manager/blog_manager/statics/js/apps/article/component.js
+++ blog_manager/blog_manager/statics/js/apps/article/component.js
@@ -6,16 +6,24 @@ import styles from './styles.css';
 const md = new MarkdownIt();
 
 const Cell = {
+  oninit: vnode => {
+    vnode.state.span = vnode.attrs.span;
+    vnode.state.cls = vnode.attrs.cls || '';
+    vnode.state.inner = vnode.attrs.inner;
+  },
   view: vnode => {
-    return <div className={`mdl-cell mdl-cell--${vnode.attrs.span}-col ${vnode.attrs.cls || ''}`}>
-      {vnode.attrs.inner}
+    return <div className={`mdl-cell mdl-cell--${vnode.state.span}-col ${vnode.state.cls}`}>
+      {vnode.state.inner}
     </div>
   }
 };
 
 const Title = {
+  oninit: vnode => {
+    vnode.state.model = vnode.attrs.model;
+  },
   view: vnode => {
-    const model = vnode.attrs.model;
+    const model = vnode.state.model;
     const inner = <input type='text' className={styles.title}
       onchange={m.withAttr('value', v => model.data.title = v)}
       value={model.data.title}
@@ -25,8 +33,11 @@ const Title = {
 };
 
 const Tags = {
+  oninit: vnode => {
+    vnode.state.model = vnode.attrs.model;
+  },
   view: vnode => {
-    const model = vnode.attrs.model;
+    const model = vnode.state.model;
     const inner = <input type='text' className={styles.tags}
       onchange={m.withAttr('value', v => model.data.tags = v.split(','))}
       value={model.data.tags.join(',')}
@@ -36,31 +47,41 @@ const Tags = {
 };
 
 const Editor = {
+  oninit: vnode => {
+    vnode.state.model = vnode.attrs.model;
+  },
   view: vnode => {
-    const inner = <div contentEditable={true} oninput={() => console.log('input')}>
-      {vnode.attrs.model.data.body}
+    const model = vnode.state.model;
+    const inner = <div contentEditable={true} oninput={ev => {
+      model.data.body = ev.target.textContent.replace('<br>', '↵');
+    }}>
+      {m.trust(vnode.attrs.model.data.body.replace(/(↵|\r\n|\n|\r)/gm, '<br>'))}
     </div>
-    return <Cell span={6} cls={styles.editor} inner={inner} />
+    return <Cell span={6} cls={`${styles.editor} ${styles.editorLeftSide}`} inner={inner} />
   }
 };
 
 const Preview = {
+  oninit: vnode => {
+    vnode.state.model = vnode.attrs.model;
+  },
   view: vnode => {
-    return <Cell span={6} cls={styles.preview} inner={
-      m.trust(md.render(vnode.attrs.model.data.body))
-    } />
+    return <Cell span={6} cls={`${styles.editor} ${styles.editorRightSide}`} inner={m.trust(md.render(vnode.state.model.data.body))} />
   }
 };
 
 const Save = {
+  oninit: vnode => {
+    vnode.state.model = vnode.attrs.model;
+  },
   view: vnode => {
-    const model = vnode.model;
+    const model = vnode.state.model;
     const inner = <button
       className='mdl-button mdl-js-button mdl-button--raised mdl-button--colored'
       onclick={model.save}>
       save
     </button>
-    return <Cell span={12} inner={inner} />
+    return <Cell span={12} cls={styles.saveButton} inner={inner} />
   }
 };
 
@@ -73,8 +94,10 @@ export default {
       return <div className='mdl-grid'>
         <Title model={model} />
         <Tags model={model} />
-        <Editor model={model} />
-        <Preview model={model} />
+        <div className={styles.editorWrap}>
+          <Editor model={model} />
+          <Preview model={model} />
+        </div>
         <Save model={model} />
       </div>
     }
