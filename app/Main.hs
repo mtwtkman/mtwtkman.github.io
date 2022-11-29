059712f78f10f0ce50e5009792b6bf78485aa333
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2022-11-29 22:39:17 +0900

    type

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,25 @@
 module Main where
 
+import qualified Data.Text as T
+import Data.Time (UTCTime)
+
+newtype NewOptions = NewOptions String
+
+data Command
+  = New NewOptions
+  | Build
+
+newtype Slug = Slug String
+
+newtype Tag = Tag String
+
+data Entry = Entry
+  { unTitle :: T.Text,
+    unBody :: T.Text,
+    unSlug :: Slug,
+    unTags :: [Tag],
+    unPublishedAt :: UTCTime
+  }
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = print ""
